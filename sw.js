a21d91b166f73885e211274bb8f3b3cb2c3289bb
serhii-shandiuk <serhii.shandiuk@gmail.com>
2026-03-28 12:16:30 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,11 @@
-const CACHE_NAME = 'pl-cards-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192x192.png',
-  './icon-512x512.png'
-];
+// No caching - always fetch fresh from network
+// Learned words are saved in localStorage (independent of SW)
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
